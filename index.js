8bf062f20cf240774b5855858e83a59bc35397c7
Matteo.Ioan <matteo.ioan@stud.itsaltoadriatico.it>
2023-12-16 21:26:53 +0100

    Upload local work on GitHub

---
+++ index.js
@@ -0,0 +1,132 @@
+let menu = document.querySelector('.inventory-menu')
+
+var isMenuVisible = false;
+
+let player = {
+    Health: document.querySelector('#player-health'),
+    Power: document.querySelector('#player-power'),
+    Defence: document.querySelector('#player-defence'),
+    BuffDef: 1.5,
+    isDefending: false,
+    actions: {
+        Attack: document.querySelector('#player-attack'),
+        Block: document.querySelector('#player-block'),
+        Inventory: document.querySelector('#player-inventory')
+    },
+    Inventory: [
+        HfPotion = 10,
+        DefPotion = 2,
+        PowPotion = 3
+    ],
+}
+
+let boss = {
+    Health: document.querySelector('#boss-health'),
+    Power: document.querySelector('#boss-power'),
+    Defence: document.querySelector('#boss-defence'),
+    BuffDef: 2.5,
+    Inventory: [
+        HfPotion = 10,
+        DefPotion = 2,
+        PowPotion = 3
+    ],
+}
+
+document.querySelector('#hf').innerHTML = player.Inventory[0]
+document.querySelector('#def').innerHTML = player.Inventory[1]
+document.querySelector('#pow').innerHTML = player.Inventory[2]
+
+player.Health.innerHTML = 100
+player.Power.innerHTML = 5
+player.Defence.innerHTML = 1.5
+
+boss.Health.innerHTML = 500
+boss.Power.innerHTML = 10
+boss.Defence.innerHTML = 3;
+
+
+player.actions.Attack.addEventListener('click', _ => {
+    lowerShield(player);
+
+    if (parseFloat(boss.Defence.innerHTML) < parseFloat(player.Power.innerHTML))
+        boss.Health.innerHTML -= parseFloat(player.Power.innerHTML - boss.Defence.innerHTML);
+
+    bossAction(searchValidNumber())
+})
+
+player.actions.Block.addEventListener('click', _ => {
+    riseShield(player);
+
+    bossAction(searchValidNumber())
+})
+
+player.actions.Inventory.addEventListener('click', _ => {
+    if (!isMenuVisible) {
+        menu.style.left = 0;
+        isMenuVisible = true
+        return
+    }
+    menu.style.left = '-100%'
+    isMenuVisible = false
+})
+
+let bossAction = (bossMove) => {
+    switch (bossMove) {
+        case '1'://Attack
+            lowerShield(boss)
+            if (parseFloat(player.Defence.innerHTML) < parseFloat(boss.Power.innerHTML))
+                player.Health.innerHTML -= parseFloat(boss.Power.innerHTML - player.Defence.innerHTML);
+            break;
+        case '2'://Defend
+            riseShield(boss)
+            break;
+        case '3'://Use Item
+            break;
+    }
+    if (player.Health.innerHTML <= 0) {
+        alert('Game over')
+    } else if (boss.Health.innerHTML <= 0) {
+        alert('You won!')
+    }
+}
+
+let lowerShield = (entity) => {
+    if (entity.isDefending) {
+        entity.Defence.innerHTML = parseFloat(entity.Defence.innerHTML) - entity.BuffDef
+        entity.isDefending = false
+    }
+}
+
+let riseShield = (entity) => {
+    if (!entity.isDefending) {
+        entity.Defence.innerHTML = parseFloat(entity.Defence.innerHTML) + entity.BuffDef
+        entity.isDefending = true;
+    }
+}
+
+let useItem = (index) => {
+    switch (index) {
+        case 0:
+            player.Health.innerHTML = parseInt(player.Health.innerHTML) + player.Inventory[index]
+            break;
+        case 1:
+            player.Defence.innerHTML = parseInt(player.Defence.innerHTML) + player.Inventory[index]
+            break;
+        case 2:
+            player.Power.innerHTML = parseInt(player.Power.innerHTML) + player.Inventory[index]
+            break;
+    }
+    bossAction(searchValidNumber())
+}
+
+let searchValidNumber = _ => {
+    random = null
+    while (random === null) {
+        random = Math.random().toFixed(1).charAt(2)
+
+        for (let i = 1; i <= player.Inventory.length; i++) {
+            if (random == i) return random;
+        }
+        random = null;
+    }
+}
